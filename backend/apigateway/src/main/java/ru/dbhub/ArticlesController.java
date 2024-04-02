737e0f42578aa2953e59bc158ee1340b7f0baf16
Marat Satskevich <taram.msatskevich@gmail.com>
2024-04-02 17:34:12 +0300

    -

--- backend/apigateway/src/main/java/ru/dbhub/ArticlesController.java
+++ backend/apigateway/src/main/java/ru/dbhub/ArticlesController.java
@@ -1,7 +1,5 @@
 package ru.dbhub;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
