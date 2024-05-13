eea282d572717d9321c32b357a58b058ff236dd4
Marat Satskevich <taram.msatskevich@gmail.com>
2024-05-13 03:39:43 +0300

    -

--- frontend/app/component/Feed.tsx
+++ frontend/app/component/Feed.tsx
@@ -14,6 +14,13 @@ interface ArticleAndInfo {
     isShown: boolean,
 }
 
+interface ArticleToShow {
+    article: Article,
+    isShown: boolean,
+    isTop: boolean,
+    idx: number,
+}
+
 const NO_BOUND_ID = -1
 const UPDATE_INTERVAL = 5000
 const PAGE_ARTICLES_COUNT = 2
@@ -83,9 +90,7 @@ export default function Feed() {
                     boundId: newBoundId,
                     loadingNow: false,
                 }
-                for (let idx = newArticles.length - 1; idx >= 0; --idx) {
-                    newTop.articles.push({article: newArticles[idx], isShown: false})
-                }
+                newArticles.forEach(article => newTop.articles.push({article: article, isShown: false}))
                 setTop(newTop)
             })
             .catch(() => {
@@ -143,10 +148,14 @@ export default function Feed() {
 
     useEffect(loadNextPage, [])
 
-    let topArticlesReversed: ArticleAndInfo[] = []
-    for (let idx = top.articles.length - 1; idx >= 0; --idx) {
-        topArticlesReversed.push(top.articles[idx])
+    let articlesToShow: ArticleToShow[] = []
+    for (let idx = 0; idx < pages.articles.length; ++idx) {
+        articlesToShow.push({article: pages.articles[idx].article,  isShown: pages.articles[idx].isShown, isTop: false, idx: idx})
     }
+    for (let idx = 0; idx < top.articles.length; ++idx) {
+        articlesToShow.push({article: top.articles[idx].article,  isShown: top.articles[idx].isShown, isTop: false, idx: idx})
+    }
+    articlesToShow.sort((article1, article2) => article2.article.timestamp - article1.article.timestamp)
 
     const toggleArticle = (idx: number, isTop: boolean) => {
         const doToggle = (idx: number, articles: ArticleAndInfo[]) => {
@@ -156,37 +165,48 @@ export default function Feed() {
         }
 
         if (isTop) {
-            setTop({...top, articles: doToggle(top.articles.length - idx, top.articles)})
+            setTop({...top, articles: doToggle(top.articles.length - idx - 1, top.articles)})
         } else {
             setPages({...pages, articles: doToggle(idx, pages.articles)})
         }
     };
 
-    const showArticles = (articles: ArticleAndInfo[], isTop: boolean) => {
-        return articles.map(
-            (article, idx) =>
-                <li key={idx + (isTop ? 0 : top.articles.length)} className={"article " + (article.isShown ? "shownarticle" : "")}>
-                    <div className="articletitle" onClick={() => toggleArticle(idx, isTop)}>
-                        <div className="articletitledate">{new Date(article.article.timestamp * 1000).toLocaleString("ru-RU")}</div>
-                        <div className="articletitletitle">
-                            <span>{article.article.source}: </span>{article.article.title}
-                        </div>
-                    </div>
-                    <div className="articletext">
-                        <p>{article.article.text}</p>
-                        <a href={article.article.link} target="_blank" rel="noopener noreferrer">
-                            <button>Открыть</button>
-                        </a>
-                    </div>
-                </li>
-        );
-    }
+    const getPreparedArticleText = (text: string) => {
+        const trimmed = text.trim().slice(0, 1000);
+        let dots = 0;
+        for (let idx = trimmed.length - 1; idx >= 0; --idx) {
+            if (trimmed[idx] != '.') {
+                break;
+            }
+            ++dots;
+        }
+
+        dots = Math.min(3, dots);
+        return trimmed + ".".repeat(3 - dots);
+    };
 
     return (
         <>
             <ul className="articleslist">
-                { showArticles(topArticlesReversed, true) }
-                { showArticles(pages.articles, false) }
+                {
+                    articlesToShow.map(
+                        (article, idx) =>
+                            <li key={idx} className={"article " + (article.isShown ? "shownarticle" : "")}>
+                                <div className="articletitle" onClick={() => toggleArticle(article.idx, article.isTop)}>
+                                    <div className="articletitledate">{new Date(article.article.timestamp * 1000).toLocaleString("ru-RU")}</div>
+                                    <div className="articletitletitle">
+                                        <span>{article.article.source}: </span>{article.article.title}
+                                    </div>
+                                </div>
+                                <div className="articletext">
+                                    <p>{getPreparedArticleText(article.article.text)}</p>
+                                    <a href={article.article.link} target="_blank" rel="noopener noreferrer">
+                                        <button>Открыть</button>
+                                    </a>
+                                </div>
+                            </li>
+                    )
+                }
             </ul>
             <button onClick={loadNextPage}>Еще</button>
         </>
